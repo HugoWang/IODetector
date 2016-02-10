9f260bfdb43aba73b75f11fbe23539a186cb8782
HugoWang <wk.oulu@gmail.com>
2016-02-10 21:28:07 +0200

    Compass works

--- app/src/main/java/com/example/kai/iodetector/Compass.java
+++ app/src/main/java/com/example/kai/iodetector/Compass.java
@@ -3,26 +3,77 @@ package com.example.kai.iodetector;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
 public class Compass extends AppCompatActivity implements SensorEventListener {
 
     ImageView znzImage;
+    float currentDegree = 0f;
+    SensorManager mSensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
+
+        znzImage = (ImageView) findViewById(R.id.znzImage);
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
     }
 
     @Override
+    protected void onResume()
+    {
+        super.onResume();
+        mSensorManager.registerListener(this,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        mSensorManager.unregisterListener(this);
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        mSensorManager.unregisterListener(this);
+        super.onStop();
+    }
+
+
+    @Override
     public void onSensorChanged(SensorEvent event) {
+
+        int sensorType = event.sensor.getType();
+        switch (sensorType)
+        {
+            case Sensor.TYPE_ORIENTATION:
+                // 获取绕Z轴转过的角度。
+                float degree = event.values[0];
+                // 创建旋转动画（反向转过degree度）
+                RotateAnimation ra = new RotateAnimation(currentDegree,
+                        -degree, Animation.RELATIVE_TO_SELF, 0.5f,
+                        Animation.RELATIVE_TO_SELF, 0.5f);
+                // 设置动画的持续时间
+                ra.setDuration(200);
+                // 运行动画
+                znzImage.startAnimation(ra);
+                currentDegree = -degree;
+                break;
+        }
+
 
     }
 
