ed15ebe6c9312a469dcd39280ea40575ef97bb76
Kai Wang <wk.oulu@gmail.com>
2016-03-09 22:55:01 +0200

    Add indoor maps

--- app/src/main/java/com/example/kai/iodetector/MapsActivity.java
+++ app/src/main/java/com/example/kai/iodetector/MapsActivity.java
@@ -1,14 +1,21 @@
 package com.example.kai.iodetector;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -16,23 +23,56 @@ import com.indooratlas.android.sdk.IALocation;
 import com.indooratlas.android.sdk.IALocationListener;
 import com.indooratlas.android.sdk.IALocationManager;
 import com.indooratlas.android.sdk.IALocationRequest;
+import com.indooratlas.android.sdk.IARegion;
+import com.indooratlas.android.sdk.resources.IAFloorPlan;
+import com.indooratlas.android.sdk.resources.IALatLng;
+import com.indooratlas.android.sdk.resources.IAResourceManager;
+
+import com.indooratlas.android.sdk.resources.IAResult;
+import com.indooratlas.android.sdk.resources.IAResultCallback;
+import com.indooratlas.android.sdk.resources.IATask;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
+import com.squareup.picasso.Target;
+
 
 public class MapsActivity extends FragmentActivity{
 
+    private boolean mCameraPositionNeedsUpdating=true;
+    private IALocationManager mIALocationManager;
+    private GoogleMap mMap;
+    private Marker mMarker;
+    private IAResourceManager mResourceManager;
+    private GroundOverlay mGroundOverlay;
+    private Target mLoadTarget;
+    private IATask<IAFloorPlan> mFetchFloorPlanTask;
+
+    /* used to decide when bitmap should be downscaled */
+    private static final int MAX_DIMENSION = 2048;
+
     private IALocationListener  mIALocationListener = new IALocationListener() {
+
+
 
         // Called when the location has changed.
         @Override
         public void onLocationChanged(IALocation location) {
             LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+            if (mMap == null) {
+                return;
+            }
             if (mMarker == null) {
-                if (mMap != null) {
-                    mMarker = mMap.addMarker(new MarkerOptions().position(latLng)
-                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.0f));
-                }
-            } else {
+                mMarker = mMap.addMarker(new MarkerOptions().position(latLng)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+            }
+            else {
                 mMarker.setPosition(latLng);
+            }
+
+            if (mCameraPositionNeedsUpdating) {
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.5f));
+                mCameraPositionNeedsUpdating = false;
             }
 
             Log.d("TEST", "Latitude: " + location.getLatitude());
@@ -45,38 +85,181 @@ public class MapsActivity extends FragmentActivity{
         }
     };
 
-    private IALocationManager mIALocationManager;
 
-    private GoogleMap mMap;
-    private Marker mMarker;
+
+    private IARegion.Listener mRegionListener = new IARegion.Listener() {
+
+        @Override
+        public void onEnterRegion(IARegion region) {
+
+            if (region.getType() == IARegion.TYPE_UNKNOWN) {
+                Toast.makeText(getApplicationContext(), "Moved out of map",
+                        Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            // entering new region, mark need to move camera
+            mCameraPositionNeedsUpdating = true;
+
+            final String newId = region.getId();
+
+            Toast.makeText(getApplicationContext(), newId, Toast.LENGTH_SHORT).show();
+            fetchFloorPlan(newId);
+        }
+
+        @Override
+        public void onExitRegion(IARegion region) {
+            if (mMarker != null) {
+                mMarker.remove();
+                mMarker = null;
+            }
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        findViewById(android.R.id.content).setKeepScreenOn(true);
 
         mIALocationManager = IALocationManager.create(this);
-        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-
+        mResourceManager = IAResourceManager.create(this);
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+        }
         mIALocationManager.requestLocationUpdates(IALocationRequest.create(), mIALocationListener);
+        mIALocationManager.registerRegionListener(mRegionListener);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         mIALocationManager.removeLocationUpdates(mIALocationListener);
+        mIALocationManager.registerRegionListener(mRegionListener);
     }
 
     @Override
     protected void onDestroy() {
         mIALocationManager.destroy();
         super.onDestroy();
+    }
+
+    private void setupGroundOverlay(IAFloorPlan floorPlan, Bitmap bitmap) {
+
+        if (mGroundOverlay != null) {
+            mGroundOverlay.remove();
+        }
+
+        if (mMap != null) {
+            BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(bitmap);
+            IALatLng iaLatLng = floorPlan.getCenter();
+            LatLng center = new LatLng(iaLatLng.latitude, iaLatLng.longitude);
+            GroundOverlayOptions fpOverlay = new GroundOverlayOptions()
+                    .image(bitmapDescriptor)
+                    .position(center, floorPlan.getWidthMeters(), floorPlan.getHeightMeters())
+                    .bearing(floorPlan.getBearing());
+
+            mGroundOverlay = mMap.addGroundOverlay(fpOverlay);
+        }
+    }
+
+    private void fetchFloorPlanBitmap(final IAFloorPlan floorPlan) {
+
+        final String url = floorPlan.getUrl();
+
+        if (mLoadTarget == null) {
+            mLoadTarget = new Target() {
+
+                @Override
+                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                    Log.d("TEST", "onBitmap loaded with dimensions: " + bitmap.getWidth() + "x"
+                            + bitmap.getHeight());
+                    setupGroundOverlay(floorPlan, bitmap);
+                }
+
+                @Override
+                public void onPrepareLoad(Drawable placeHolderDrawable) {
+                    // N/A
+                }
+
+                @Override
+                public void onBitmapFailed(Drawable placeHolderDraweble) {
+                    Toast.makeText(getApplicationContext(), "Failed to load bitmap",
+                            Toast.LENGTH_SHORT).show();
+                }
+            };
+        }
+
+        RequestCreator request = Picasso.with(this).load(url);
+
+        final int bitmapWidth = floorPlan.getBitmapWidth();
+        final int bitmapHeight = floorPlan.getBitmapHeight();
+
+        if (bitmapHeight > MAX_DIMENSION) {
+            request.resize(0, MAX_DIMENSION);
+        } else if (bitmapWidth > MAX_DIMENSION) {
+            request.resize(MAX_DIMENSION, 0);
+        }
+
+        request.into(mLoadTarget);
+    }
+
+    /**
+     * Fetches floor plan data from IndoorAtlas server.
+     */
+    private void fetchFloorPlan(String id) {
+
+        // if there is already running task, cancel it
+        cancelPendingNetworkCalls();
+
+        final IATask<IAFloorPlan> task = mResourceManager.fetchFloorPlanWithId(id);
+
+        task.setCallback(new IAResultCallback<IAFloorPlan>() {
+
+            @Override
+            public void onResult(IAResult<IAFloorPlan> result) {
+
+                if (result.isSuccess() && result.getResult() != null) {
+                    // retrieve bitmap for this floor plan metadata
+                    fetchFloorPlanBitmap(result.getResult());
+                } else {
+                    // ignore errors if this task was already canceled
+                    if (!task.isCancelled()) {
+                        // do something with error
+                        Toast.makeText(getApplicationContext(),
+                                "loading floor plan failed: " + result.getError(), Toast.LENGTH_LONG)
+                                .show();
+                        // remove current ground overlay
+                        if (mGroundOverlay != null) {
+                            mGroundOverlay.remove();
+                            mGroundOverlay = null;
+                        }
+                    }
+                }
+            }
+        }, Looper.getMainLooper()); // deliver callbacks using main looper
+
+        // keep reference to task so that it can be canceled if needed
+        mFetchFloorPlanTask = task;
+
+    }
+
+    /**
+     * Helper method to cancel current task if any.
+     */
+    private void cancelPendingNetworkCalls() {
+        if (mFetchFloorPlanTask != null && !mFetchFloorPlanTask.isCancelled()) {
+            mFetchFloorPlanTask.cancel();
+        }
     }
 
 }
